7ab0ba05c276d5eb1601adaa5e73e13d6483f3af
frostianYoung <1700012863@pku.edu.cn>
2021-05-11 16:07:30 +0800

    Initial Commit

---
+++ pages/login/login.js
@@ -0,0 +1,22 @@
+// pages/login/login.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+  loadPages: function(param){
+    wx.switchTab({
+      url: '/pages/association/association',
+    })
+  }
+})
